6a5d4301aeae641c06e0b8a559d0b15df8b1389e
bwl1 <52504367+bwl1@users.noreply.github.com>
2019-07-06 06:56:50 -0500

    7.5作业 摩尔斯编码仿真

---
+++ morse_new/morse_new.ino
@@ -0,0 +1,114 @@
+char Morse[][4] = {
+    {'.','-',' ',' '},      //A
+    {'-','.','.','.'},      //B
+    {'-','.','-','.'},      //C
+    {'-','.','.',' '},      //D
+    {'.',' ',' ',' '},      //E
+    {'.','.','-','.'},      //F
+    {'-','-','.',' '},      //G
+    {'.','.','.','.'},      //H
+    {'.','.',' ',' '},      //I
+    {'.','-','-','-'},      //J
+    {'-','.','-',' '},      //K
+    {'.','-','.','.'},      //L
+    {'-','-',' ',' '},      //M
+    {'-','.',' ',' '},      //N
+    {'-','-','-',' '},      //O
+    {'.','-','-','.'},      //P
+    {'-','-','.','-'},      //Q
+    {'.','-','.',' '},      //R
+    {'.','.','.',' '},      //S
+    {'-',' ',' ',' '},      //T
+    {'.','.','-',' '},      //U
+    {'.','.','.','-'},      //V
+    {'.','-','-',' '},      //W
+    {'-','.','.','-'},      //X
+    {'-','.','-','-'},      //Y
+    {'-','-','.','.'}       //Z
+    
+}; //摩尔斯码表
+
+int LED = 13;
+int dotTime = 250; //单位时间0.25s
+int i,t,serialFlag,cnt;
+
+
+void setup()
+{
+    
+    Serial.begin(9600);
+    pinMode(LED, OUTPUT);
+    //Initialize all peripherals
+}
+
+void loop()
+{
+   
+    String str = "";
+    String morse_code = "";
+    cnt=0;
+
+    while (Serial.available() > 0)
+    {
+        serialFlag = 1;
+        str += char(Serial.read());
+        delay(1);
+        cnt++;
+    }
+
+    if(serialFlag)
+    {
+        for (i = 0; i < cnt; i++)
+        {
+            for (t = 0; t < 4; t++)
+            {
+                if (str[i] >= 97 && str[i] <= 122) //小写字母
+                    morse_code += char(Morse[int(str[i] - 97)][t]); //查表
+            }
+            morse_code += ' ';
+        }
+        
+        for(i = 0; i < cnt; i++)
+        {
+            for (t = 0; t < 4; t++)
+            {
+                switch(morse_code[t+i*4+i-1])
+                {
+                    case '-': blinkDash(LED, dotTime);  break;
+                    case '.': blinkDot(LED, dotTime); break;
+                    case ' ': blink_W_Space(LED, dotTime);  break;
+                }
+                blink_C_Space(LED, dotTime); //字符结束
+            }
+        }
+
+    }
+    //delay(1);
+}
+void blinkDot(int _pin, int _dottime)
+{
+  digitalWrite(_pin,HIGH);
+  delay(_dottime);
+  digitalWrite(_pin,LOW);
+  delay(_dottime);
+}
+
+void blinkDash(int _pin, int _dottime)
+{
+  digitalWrite(_pin,HIGH);
+  delay(_dottime*4);
+  digitalWrite(_pin,LOW);
+  delay(_dottime);
+}
+
+void blink_C_Space(int _pin, int _dottime)
+{
+  digitalWrite(_pin,LOW);
+  delay(_dottime*3);
+}
+
+void blink_W_Space(int _pin, int _dottime)
+{
+  digitalWrite(_pin,LOW);
+  delay(_dottime*7);
+}
